adb791e6a122df8470cb1fe9da936d8454f7b45c
MohamedMohamady125 <mohamadmohamady125@gmail.com>
2025-07-24 19:46:29 +0300

    🔧 Force remove persistent notification button

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "04d76bf0956b04f56212be01bca4274d",
+const RESOURCES = {"flutter_bootstrap.js": "fcf424fce66bbbbdb619928df6ddba1b",
 "version.json": "39a4fbe1894389a54bf2a20f130c80ad",
 "index.html": "99606990c53064607a0b76f97dcff432",
 "/": "99606990c53064607a0b76f97dcff432",
